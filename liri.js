3f258bc037d537bc9c2a2eeb91b9891081037713
noisette <kasusky.music@gmail.com>
2020-06-14 02:25:24 -0700

    Successfuly random function for Spotify

--- liri.js
+++ liri.js
@@ -30,7 +30,7 @@ function liriChoice (selectedApp, userInput) {
             searchMovie(userInput);
             break;
         case "do-what-it-says":
-            searchRandom(userInput);
+            searchRandom();
             break;
         default:
             console.log("Not a valid argument. Please try again:" + 
@@ -76,9 +76,9 @@ function searchSong(userInput) {
         // console.log(response);
         // console.log(userInput);
         console.log("\nArtist(s): " + response.tracks.items[0].album.artists[0].name);
-        console.log("\nSong name: "+ response.tracks.items[0].name);
-        console.log("\nPreview link: "+ response.tracks.items[0].href);
-        console.log("\nFrom the album: "+ response.tracks.items[0].album.name);
+        console.log("Song name: "+ response.tracks.items[0].name);
+        console.log("Preview link: "+ response.tracks.items[0].href);
+        console.log("From the album: "+ response.tracks.items[0].album.name);
     })
     .catch(function(err) {
         console.log(err);
@@ -98,9 +98,10 @@ function searchMovie(userInput) {
     axios.get(movieQueryURL)
     .then(function(response) {
         //   console.log(response)
-          console.log("Movie Title: " + response.data.Title);
+          console.log("\nMovie Title: " + response.data.Title);
           console.log("Year released: " + response.data.Year);
           console.log("IMBD rating: " + response.data.imbdRating);
+        //   Ratings aren't available??
         //   console.log("Rotten Tomatoes rating: " + response.data.Ratings);
           console.log("Country produced: " + response.data.Country);
           console.log("Language: " + response.data.Language);
@@ -114,7 +115,23 @@ function searchMovie(userInput) {
 };
 
 // search Random by reading the random.txt file
+function searchRandom() {
+    // The code will store the contents of the reading inside the variable "data"
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
 
+        if (data) {
+            // console.log(data)
+            var dataArr = data.split(",");
+            selectedApp = dataArr[0];
+            userInput = dataArr[1];
+            liriChoice(selectedApp, userInput)
+        }
 
+    })
+};
 
 liriChoice(selectedApp, userInput);
